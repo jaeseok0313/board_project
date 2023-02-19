86d343ac155726c339e0e576756a148c72d265d0
DESKTOP-UR8E42A\whwod <whwodlr5472@gmail.com>
2023-02-19 16:42:09 +0900

    Initial commit

---
+++ src/main/java/com/gma/gmagame/GmagameApplication.java
@@ -0,0 +1,13 @@
+package com.gma.gmagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmagameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmagameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gma/gmagame/GmagameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gma.gmagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
