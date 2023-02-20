c787ed6b4b8b7625d5e2dee6394b2435a92f8e6a
jaeseok <5247754@naver.com>
2023-02-20 18:37:04 +0900

    sql script

--- src/main/java/com/gma/gmagame/Service/UserService.java
+++ src/main/java/com/gma/gmagame/Service/UserService.java
@@ -1,11 +1,13 @@
 package com.gma.gmagame.Service;
 import com.gma.gmagame.model.User;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Mapper
 public interface UserService {
     public List<User> selectList();
 
